1be6e23a22c8cd18dff9448cd842327f9ac118bf
normanlrasmussen <normansawesome@gmail.com>
2025-03-12 09:11:35 -0600

    Slight modificiation

--- Netflix.py
+++ Netflix.py
@@ -1,11 +1,14 @@
 from Person import Person
 from Movie import Movie
 import pandas as pd
+import numpy as np
 
 class Netflix:
     def __init__(self):
         self.movies = []
         self.subs = []
+        self.movies_matrix = None
+        self.subs_matrix = None
 
     def random_subs(self, quantity: int):
         self.subs = []
@@ -17,9 +20,14 @@ class Netflix:
         if not isinstance(quantity, int):
             raise ValueError("Quantity must be integer")
         
-        for i in range(quantity):
+        
+        person = Person(name=names.iloc[0, 0] + " " + names.iloc[i, 1])
+        self.subs.append(person)
+        self.subs_matrix = person.tastes
+        for i in range(1, quantity):
             person = Person(name=names.iloc[i, 0] + " " + names.iloc[i, 1])
             self.subs.append(person)
+            self.subs_matrix = np.vstack((self.subs_matrix, person.tastes))
 
     def random_movies(self, quantity: int):
         self.movies = []
@@ -31,9 +39,13 @@ class Netflix:
         if not isinstance(quantity, int):
             raise ValueError("Quantity must be integer")
         
-        for i in range(quantity):
+        movie = Movie(name=movies.iloc[0, 0])
+        self.movies.append(movie)
+        self.movies_matrix = movie.genre_array
+        for i in range(1, quantity):
             movie = Movie(name=movies.iloc[i, 0])
             self.movies.append(movie)
+            self.movies_matrix = np.vstack((self.movies_matrix, movie.genre_array))
 
 
 #TODO Find people that like a certain genre
