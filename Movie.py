6a19ffb67cc55a572f83414852e3d019dca138fe
normanlrasmussen <normansawesome@gmail.com>
2025-03-11 21:38:02 -0600

    Started the movie class

--- Movie.py
+++ Movie.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Person:
+class Movie:
     def __init__(self, randomize=True):
         if randomize:
             self.randomize()
@@ -8,7 +8,7 @@ class Person:
             self.movies_watched = np.zeros(11) #The amount of movies wathced
             self.tastes = np.zeros(11) #There tastes, normalized
             self.months_on_netflix = 0 #How long they have been on netflix
-            self.movies_array = np.zeros(11) #This will be the altered movies watched list
+            self.movies_matrix = np.zeros(11) #This will be the altered movies watched list
         self.name = False #Set the name to none
         self.genre_key = { #This makes the genre key that will be used for later
             0: "Action",
@@ -30,21 +30,19 @@ class Person:
         self.like_rate = 0.02 #How much a like increases a given category
     
     def randomize(self) -> None:
-        self.movies_watched = np.random.randint(0,100,11)
-        self.movies_array = self.movies_watched
-        self.tastes = self.movies_watched/np.linalg.norm(self.movies_watched)
-        self.months_on_netflix = np.random.randint(0,24)
+        u = np.random.randint(0,100,11)
+        self.genre_array = u/np.linalg.norm(u)
 
-    def favorite_genre(self) -> str:
-        indices = np.where(self.tastes == self.tastes.max())[0]
+    def movie_genre(self) -> str:
+        indices = np.where(self.genre_array == self.genre_array.max())[0]
         if len(indices) == 1:
             return self.genre_key[indices[0]]
         else:
-            tastes = self.genre_key[indices[0]]
+            genres = self.genre_key[indices[0]]
             indices = indices[1::]
             for indice in indices:
-                tastes += f", {self.genre_key[indice]}"
-            return tastes
+                genres += f", {self.genre_key[indice]}"
+            return genres
     
     def set_name(self, name: str) -> None:
         self.name = name
@@ -60,7 +58,5 @@ class Person:
 
 
 #TODO add a decay factor/rate with new shows are watched
-    #Can do it by multiplying movie_matrix by the decay rate and then adding one for the movie watched
 #TODO add a function to indicate they have wathced a show
-    #Can do it by multiplying the movies_matrix by a like_rate
 
